66e92fc593b36964d27909145c2bf1fb001f6eb2
PaintRock <6126@holbertonstudents.com>
2023-09-07 16:36:47 -0500

    4v21

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,35 +1,52 @@
+// Import the Currency class from 3-currency.js
 import Currency from './3-currency';
 
-export default class Pricing {
-    constructor(amount, Currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
- //ge(tters and setters
- get amount() {
-    return this._amount;
- }
-
- set amount(amount) {
+class Pricing {
+  constructor(amount, currency) {
     this._amount = amount;
- }
-
- get currency() {
-    return this._currency;
- }
-
- set currency(currency) {
     this._currency = currency;
- }
- //Construct method to display the full price format
- displayFullPrice() {
-    return '${this._amount} ${this._currency.name} (${this._currency.code})';
- }
-
- //Methode to convert a price 
- static convertPrice(amount, conversionRate) {
-  if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new Error('Both amount and conversionRate must be numbers');
   }
+
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  // Method to display the full price format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert a price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Both amount and conversionRate must be numbers');
+    }
+
     return amount * conversionRate;
- }
+  }
+}
+
+export default Pricing;
+
+// Example usage:
+const dollar = new Currency('$', 'Dollars');
+const price = new Pricing(50, dollar);
+
+console.log(price.displayFullPrice()); // Output: 50 Dollars ($)
+
+const convertedAmount = Pricing.convertPrice(50, 0.85); // Convert 50 Dollars to another currency at a conversion rate of 0.85
+console.log(`Converted Amount: ${convertedAmount}`); // Output: Converted Amount: 42.5
