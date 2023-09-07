5ff76f351ea7d62ef3898485a78d0553f9e5d807
PaintRock <6126@holbertonstudents.com>
2023-09-07 01:37:21 -0500

    oneV2lint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
 import ClassRoom from "./0-classroom.js";
-function initializeRooms () {
-  const sizes = [19, 20, 34]
-  const rooms = sizes.map((size)=> new ClassRoom(size));
+
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map((size) => new ClassRoom(size));
   return rooms;
 }
 export default initializeRooms;
