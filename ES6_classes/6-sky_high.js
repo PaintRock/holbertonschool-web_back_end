5e84862cf757066136125b9148ad9f35da1b2870
PaintRock <6126@holbertonstudents.com>
2023-09-07 17:22:56 -0500

    sixV2

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,16 +1,16 @@
-import Building from '5-building';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        this._sqft = sqft;
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get sqft {
-        return this._sqft;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    get floors {
-        return this._floors;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
