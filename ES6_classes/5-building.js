e1e3133d4499b065baa3adca717ee9d9805b1a6b
PaintRock <6126@holbertonstudents.com>
2023-09-07 17:08:04 -0500

    fiveVfive

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -6,29 +6,15 @@ export default class Building {
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method that must be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number.');
+    } else {
+      this._sqft = newSqft;
+    }
   }
 }
-
-// Example usage:
-class OfficeBuilding extends Building {
-  constructor(sqft, officeCount) {
-    super(sqft);
-    this._officeCount = officeCount;
-  }
-
-  // Override the abstract method
-  evacuationWarningMessage() {
-    return `Evacuate the office building with ${this._officeCount} offices!`;
-  }
-}
-
-const officeBuilding = new OfficeBuilding(5000, 10);
-console.log(officeBuilding.evacuationWarningMessage()); // Output: Evacuate the office building with 10 offices!
