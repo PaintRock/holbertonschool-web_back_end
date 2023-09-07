6783d6f7bded44fd35cb83e9c8faad203cd9bfae
PaintRock <6126@holbertonstudents.com>
2023-09-07 17:20:28 -0500

    sixV1

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from '5-building';
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        this._sqft = sqft;
+        this._floors = floors;
+    }
+
+    get sqft {
+        return this._sqft;
+    }
+
+    get floors {
+        return this._floors;
+    }
+}
