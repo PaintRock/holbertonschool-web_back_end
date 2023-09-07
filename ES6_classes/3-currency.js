d3d56c3567a6d6e468aec3501998f0115cc92673
PaintRock <6126@holbertonstudents.com>
2023-09-07 15:57:23 -0500

    threeVone

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -2,8 +2,33 @@ export default class Currency {
   constructor(code, name) {
     this._name = name;
     this._code = code;
-    }
-  displayFullCurrency() {
-		return '${this._name} (${this._code})';
+  }
+  
+	displayFullCurrency() {
+    return '${this._name} (${this._code})';
+  }
+
+	get code () {
+		return this._code
+	}	
+
+	set code (mycode) {
+		if (typeof mycode !== 'string') {
+			throw new TypeError ('Code must be a string.')
 	}
-}
+else {
+	this._code = mycode;
+	}
+
+	get name() {
+		return this._name
+	}
+
+	set name(myName) {
+		if (typeof myName !== 'string') {
+			throw new TypeError('Name must be a string.'); 
+	} else {
+		this._name = myName;
+	}
+	}
+	}
