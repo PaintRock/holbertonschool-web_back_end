fd3aefe985bc9dba79c4d618b23d56e8380c3959
PaintRock <6126@holbertonstudents.com>
2023-09-12 18:47:49 -0500

    zerov6

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,13 @@
 function getListStudents() {
-    const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
   
-    return students;
-  }
-  
-  // Example usage:
-  const studentList = getListStudents();
-  console.log(studentList);
-  
+  return students;
+}
+
+// Example usage:
+const studentList = getListStudents();
+console.log(studentList);
