a6a102209a442cf89feb8f596d3e5344b10b32d6
PaintRock <6126@holbertonstudents.com>
2023-09-13 13:51:48 -0500

    9vstructure

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,10 @@
-def groceriesList():
-    grocery_items = {
-        "Apples": 10,
-        "Tomatoes": 10,
-        "Pasta": 1,
-        "Rice": 1,
-        "Banana": 5
-    }
-    return grocery_items
-
-# Example usage:
-grocery_list = groceriesList()
-print(grocery_list)
+export default function groceriesList():{
+    const grocery_items = [
+        ["Apples": 10,]
+        ["Tomatoes": 10,]
+        ["Pasta": 1,]
+        ["Rice": 1,]
+        ["Banana": 5]
+    ]
+    return Map(grocery_items);
+}
