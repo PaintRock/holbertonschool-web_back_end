c753186212cae6c91ee0d0758868f8c2584c65cb
PaintRock <6126@holbertonstudents.com>
2023-09-13 13:16:05 -0500

    4lint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,16 +1,15 @@
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName: firstName,
-      lastName: lastName
-    });
-  }
-  
-  // Example usage:
-  signUpUser('John', 'Doe')
-    .then((user) => {
-      console.log('User signed up:', user);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName
+  });
+}
+
+// Example usage:
+signUpUser('John', 'Doe')
+  .then((user) => {
+    console.log('User signed up:', user);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
