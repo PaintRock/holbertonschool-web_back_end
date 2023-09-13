76d47580662dcbc3942768cdfbabd938158728b3
PaintRock <6126@holbertonstudents.com>
2023-09-13 12:33:41 -0500

    all

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+def groceriesList():
+    grocery_items = {
+        "Apples": 10,
+        "Tomatoes": 10,
+        "Pasta": 1,
+        "Rice": 1,
+        "Banana": 5
+    }
+    return grocery_items
+
+# Example usage:
+grocery_list = groceriesList()
+print(grocery_list)
