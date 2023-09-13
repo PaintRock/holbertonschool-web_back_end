459961c41ad8c4bd0570ec8303925f1043351e00
PaintRock <6126@holbertonstudents.com>
2023-09-12 19:56:58 -0500

    threeV2

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,6 @@
 export default function getStudentIdsSum(students) {
   // Use the reduce function to calculate the sum of student IDs
-  const sum = students.reduce((accumulator, student) => {
-    return accumulator + student.id;
-  }, 0); // Initialize the accumulator to 0
+  const sum = students.reduce((accumulator, student) => accumulator + student.id, 0); // Initialize the accumulator to 0
 
   return sum;
 }
@@ -11,8 +9,9 @@ export default function getStudentIdsSum(students) {
 const students = [
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
 ];
 
 const totalIdsSum = getStudentIdsSum(students);
 console.log(totalIdsSum); // Output: 8 (1 + 2 + 5 = 8)
+  
