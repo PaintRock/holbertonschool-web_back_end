6a828a2f8ecb05c26736248851ca293624c38d5e
PaintRock <6126@holbertonstudents.com>
2023-09-13 13:19:37 -0500

    3lint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,17 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-const photoPromise = uploadPhoto();
-const userPromise = createUser();
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
 
   Promise.all([photoPromise, userPromise])
-  .then((results) => {
-    const [userResponse] = results;
-    console.log(`body ${userResponse.firstName} ${userResponse.lastName}`);
-  })
-  .catch(() => {
-    console.log('Signup system offline');
-  });
+    .then((results) => {
+      const [userResponse] = results;
+      console.log(`body ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 // Call handleProfileSignup to start the process
