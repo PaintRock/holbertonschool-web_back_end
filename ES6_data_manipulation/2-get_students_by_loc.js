866988dc0fa8cd3d9b64323296c59a558ffb2cfa
PaintRock <6126@holbertonstudents.com>
2023-09-12 19:40:19 -0500

    twoVone

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+export default function getStudentsByLocation(students, city) {
+  // Use the filter function to get students in the specified city
+  const studentsInCity = students.filter(student => student.location === city);
+
+  return studentsInCity;
+}
+
+// Example usage:
+const students = [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+];
+
+const city = 'San Francisco';
+const studentsInCity = getStudentsByLocation(students, city);
+console.log(studentsInCity);
