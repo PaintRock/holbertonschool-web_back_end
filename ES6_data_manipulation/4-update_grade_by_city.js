38ea478d5980761fdcf38a3c0c47cef5358be0b4
PaintRock <6126@holbertonstudents.com>
2023-09-12 20:12:51 -0500

    fourV2

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   // Filter students in the specified city
-  const studentsInCity = students.filter(student => student.location === city);
+  const studentsInCity = students.filter((student) => student.location === city);
 
   // Map students to update their grades
-  const updatedStudents = studentsInCity.map(student => {
+  const updatedStudents = studentsInCity.map((student) => {
     // Find the corresponding grade in newGrades, or set it to N/A if not found
-    const gradeObject = newGrades.find(grade => grade.studentId === student.id);
+    const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
     const grade = gradeObject ? gradeObject.grade : 'N/A';
 
     // Create a new student object with the updated grade
     return {
       ...student,
-      grade
+      grade,
     };
   });
 
@@ -22,12 +22,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 const students = [
   { id: 1, firstName: 'Guillaume', location: 'San Francisco', grade: 'A' },
   { id: 2, firstName: 'James', location: 'Columbia', grade: 'B' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 'C' }
+  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 'C' },
 ];
 
 const newGrades = [
   { studentId: 1, grade: 'A+' },
-  { studentId: 5, grade: 'B' }
+  { studentId: 5, grade: 'B' },
 ];
 
 const city = 'San Francisco';
