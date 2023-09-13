f57c0a85d3ef1e34a35dd378a102a9de21e21126
PaintRock <6126@holbertonstudents.com>
2023-09-13 12:58:28 -0500

    0v3

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,31 +1,9 @@
-export default function getResponseFromAPI() {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    // Perform your asynchronous operations here
-    // For example, you can make an HTTP request using fetch:
-    fetch('https://api.example.com/data')
-      .then((response) => {
-        if (response.ok) {
-          // If the response is successful, resolve with the data
-          response.json().then((data) => {
-            resolve(data);
-          });
-        } else {
-          // If there's an error, reject with an error message
-          reject(new Error('API request failed'));
-        }
-      })
-      .catch((error) => {
-        // Handle any other errors that might occur during the request
-        reject(error);
-      });
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
 }
-
-// Example usage:
-getResponseFromAPI()
-  .then((data) => {
-    console.log('Received data from API:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
