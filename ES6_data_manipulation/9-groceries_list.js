270d2ccb2da16cbf7c3b7e3a3ed0b6775ec75703
PaintRock <6126@holbertonstudents.com>
2023-09-13 13:54:07 -0500

    9vstructure2

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList():{
     const grocery_items = [
-        ["Apples": 10,]
-        ["Tomatoes": 10,]
-        ["Pasta": 1,]
-        ["Rice": 1,]
-        ["Banana": 5]
+        ["Apples", 10,]
+        ["Tomatoes", 10,]
+        ["Pasta", 1,]
+        ["Rice", 1,]
+        ["Banana", 5]
     ]
     return Map(grocery_items);
 }
