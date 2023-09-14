34875b6b9532652e99f993d9b019cae927a4e9bb
PaintRock <6126@holbertonstudents.com>
2023-09-14 09:32:46 -0500

    7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,22 @@
-export default function setFromArray(array) { 
-  return new Set(array);
+export default function hasValuesFromArray(set, array) {
+  // Convert the array to a set to remove duplicate values, if any
+  const arraySet = new Set(array);
+
+  // Check if all elements in the arraySet are also in the provided set
+  for (const element of arraySet) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+
+  // If all elements in the arraySet exist in the set, return true
+  return true;
 }
+
+// Example usage:
+const mySet = new Set([1, 2, 3, 4, 5]);
+const myArray = [2, 3, 4];
+
+const result = hasValuesFromArray(mySet, myArray);
+console.log(result); // Output will be true because all elements in myArray exist in mySet.
+
