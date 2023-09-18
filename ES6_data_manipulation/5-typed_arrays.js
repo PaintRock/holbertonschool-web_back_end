2f9762fe31984e2f08a66c0346a57be33c2f82ba
PaintRock <6126@holbertonstudents.com>
2023-09-17 22:04:51 -0500

    8

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,7 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
-  }
-
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-
-  int8View[position] = value;
-
-  return new DataView(buffer);
+export default function cleanSet(Set, startString) {
+  let resultString = [...Set]
+    .filter(value => value.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
+  return resultString;
 }
