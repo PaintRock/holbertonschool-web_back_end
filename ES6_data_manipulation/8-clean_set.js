71377bf5858da613d2f0397eb131d5729b8518da
PaintRock <6126@holbertonstudents.com>
2023-09-17 21:56:13 -0500

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,5 @@
-export default function cleanSet(set, startString) {
-  const result = [];
-
-  // Iterate over the set elements
-  set.forEach((value) => {
-    // Check if the value starts with startString
-    if (value.startsWith(startString)) {
-      // Append the part of the value after startString to the result array
-      result.push(value.substring(startString.length));
-    }
-  });
-
-  // Join the result array elements with hyphens to create the final string
-  return result.join('-');
+export default function cleanSet(Set, startString) {
+  const filteredValues = [...Set].filter(value => value.startsWith(startString));
+  const resultString = filteredValues.join('-');
+  return resultString;
 }
