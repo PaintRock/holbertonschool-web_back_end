34c9e6a5056006de4c76ee7ba3f90e964e885e71
PaintRock <6126@holbertonstudents.com>
2023-09-17 21:58:18 -0500

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(Set, startString) {
-  const filteredValues = [...Set].filter(value => value.startsWith(startString));
+  const filteredValues = [...Set].filter((value) => value.startsWith(startString));
   const resultString = filteredValues.join('-');
   return resultString;
 }
